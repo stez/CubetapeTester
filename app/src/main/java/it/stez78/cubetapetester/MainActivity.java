a4861ff5909d1bf884f1f1a564cb7a63479c8780
Stefano Zanotti <stefanozanotti@snps.it>
2019-02-20 17:22:11 +0100

    fixes

--- app/src/main/java/it/stez78/cubetapetester/MainActivity.java
+++ app/src/main/java/it/stez78/cubetapetester/MainActivity.java
@@ -86,21 +86,6 @@ public class MainActivity extends AppCompatActivity implements OnItemClickListen
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
     public void onItemClick(Object item) {
         PairedDevice element= (PairedDevice) item;
         Intent launchPairedDeviceActivityIntent = new Intent(this, PairedDeviceActivity.class);
